f2dbdb68465a014357630b7a809fb9e400223a8f
Eduardo Nagashima <eduyudji@gmail.com>
2022-02-09 17:40:20 -0300

    últimos ajustes do html e css

--- script/scrpit.js
+++ script/scrpit.js
@@ -1,7 +1,40 @@
+let selectedPerson = '';
+
 function sendMessage() {
     alert('Enviar mensagem');
 }
 
 function refresh() {
 
+}
+
+function selectPerson(el) {
+    let selected = document.querySelector('.selected');
+    if (selected !== null) {
+        selected.classList.remove('selected');
+    }
+    el.classList.add('selected');
+    console.log(el);
+}
+
+function selectVisibility(el) {
+    let selected = document.querySelector('.selected');
+    if (selected !== null) {
+        selected.classList.remove('selected');
+    }
+    el.classList.add('selected');
+    console.log(el);
+}
+
+
+function show(element) {
+    let el = document.querySelector('.hide');
+    let body = document.querySelector('body')
+    if (el !== null) {
+        el.classList.toggle('hide');
+        body.classList.toggle('body-fixed');
+    } else {
+        element.parentNode.classList.toggle('hide');
+        body.classList.toggle('body-fixed');
+    }
 }
